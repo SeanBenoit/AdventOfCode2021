6f878b973bfeb5d45d6f2f02feebc793197d1faa
Sean Benoit <sean.joseph.benoit@gmail.com>
2021-12-01 08:31:42 -0500

    Cleaned up solution for day 1

--- src/day1/DepthChecker.kt
+++ src/day1/DepthChecker.kt
@@ -2,24 +2,43 @@ package day1
 
 fun solvePuzzle1(input: List<String>) {
     val intInput = input.map { it.toInt() }
-    var count = 0
-    for (index in 1 until intInput.size) {
-        if (intInput[index] > intInput[index - 1]) {
-            count++
-        }
-    }
-    println(count)
+
+    val result = countIncreasing(intInput)
+
+    println(result)
 }
 
 fun solvePuzzle2(input: List<String>) {
     val intInput = input.map { it.toInt() }
+
+    val sumInput = slidingWindowSums(3, intInput)
+
+    val result = countIncreasing(sumInput)
+
+    println(result)
+}
+
+fun countIncreasing(input: List<Int>): Int {
     var count = 0
-    for (index in 3 until intInput.size) {
-        val firstSum = intInput[index - 3] + intInput[index - 2] + intInput[index - 1]
-        val secondSum = firstSum - intInput[index - 3] + intInput[index]
-        if (firstSum < secondSum) {
+    for (index in 1 until input.size) {
+        if (input[index] > input[index - 1]) {
             count++
         }
     }
-    println(count)
+    return count
+}
+
+fun slidingWindowSums(windowSize: Int, input: List<Int>): List<Int> {
+    require(input.size >= windowSize) {
+        "input must contain enough items to fill at least one window"
+    }
+    val sumList = mutableListOf<Int>()
+    for (index in windowSize - 1 until input.size) {
+        var sum = 0
+        for (windowIndex in 0 until windowSize) {
+            sum += input[index - windowIndex]
+        }
+        sumList.add(sum)
+    }
+    return sumList.toList()
 }
