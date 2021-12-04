d2b737593445d9a636c17d4c1e2dceec0713733a
Sean Benoit <sean.joseph.benoit@gmail.com>
2021-12-03 21:45:44 -0500

    Cleaned up day 3 solutions

--- src/day3/PowerDiagnostic.kt
+++ src/day3/PowerDiagnostic.kt
@@ -1,33 +1,22 @@
 package day3
 
 class PowerDiagnostic(private val rawInput: List<String>) {
+    private var bitFrequencies = mutableListOf<List<Int>>()
+
     init {
-        println(readPowerDiagnostic(rawInput))
+        readPowerDiagnostic(rawInput)
     }
 
-    private fun readPowerDiagnostic(input: List<String>): List<List<Int>> {
-        val bitFrequencies = mutableListOf<MutableList<Int>>()
-
+    private fun readPowerDiagnostic(input: List<String>) {
         for (position in input[0].indices) {
-            bitFrequencies.add(mutableListOf(0, 0))
+            val countOfZeroes = input.count { it[position] == '0' }
+            val countOfOnes = input.size - countOfZeroes
+            bitFrequencies.add(listOf(countOfZeroes, countOfOnes))
         }
-
-        for (line in input) {
-            line.forEachIndexed { index, bit ->
-                bitFrequencies[index][bit.toString().toInt()]++
-            }
-        }
-
-        return bitFrequencies
     }
 
     fun getGammaBits(): String {
-        return getGammaBitsHelper(rawInput)
-    }
-
-    private fun getGammaBitsHelper(input: List<String>): String {
         var gammaBits = ""
-        val bitFrequencies = readPowerDiagnostic(input)
 
         for (bitFrequency in bitFrequencies) {
             gammaBits += if (bitFrequency[0] > bitFrequency[1]) {
@@ -41,12 +30,7 @@ class PowerDiagnostic(private val rawInput: List<String>) {
     }
 
     fun getEpsilonBits(): String {
-        return getEpsilonBitsHelper(rawInput)
-    }
-
-    private fun getEpsilonBitsHelper(input: List<String>): String {
         var epsilonBits = ""
-        val bitFrequencies = readPowerDiagnostic(input)
 
         for (bitFrequency in bitFrequencies) {
             epsilonBits += if (bitFrequency[0] < bitFrequency[1]) {
@@ -110,9 +94,7 @@ fun solvePuzzle2(input: List<String>) {
     val powerDiagnostic = PowerDiagnostic(input)
 
     val oxygenGeneratorRating = powerDiagnostic.getOxygenGeneratorBits().toInt(2)
-    println(Integer.toBinaryString(oxygenGeneratorRating).padStart(12, '0'))
     val co2ScrubberRating = powerDiagnostic.getCo2ScrubberBits().toInt(2)
-    println(Integer.toBinaryString(co2ScrubberRating))
 
     val result = oxygenGeneratorRating * co2ScrubberRating
 
